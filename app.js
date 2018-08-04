bc6e808b5e7516cf4c11674631d605bb34d09a02
jsebastianms1 <jsebastianms1@gmail.com>
2018-08-03 21:38:13 -0500

    almacena e inicia el servidor, corregir revisar cuando ya esta insertado

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const app = express();
 const router = require('./routes');
 const bodyparser = require('body-parser');
 const jsonparser = bodyparser.json()
+const load = require('./controllers')
 
 app.use('/fifa',jsonparser,router.fifarouter);
 
-app.listen(3000, function () {
-  console.log('Servidor encendido en puerto 3000');
-});
+
+app.on('ready',function(){
+  app.listen(3000, function () {
+    console.log('Servidor encendido en puerto 3000');
+  });
+})
+
+load.fifaController.loadSave().then(()=>{
+  let executed=false;
+  if(!executed){
+    executed=true;
+    app.emit('ready')
+  }
+})
+
